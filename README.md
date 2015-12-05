bbebd28484e23d9b3e541208b0c85e448093992b
nelsonic <contact.nelsonic+github@gmail.com>
2015-12-05 16:40:05 +0000

    adds example render function for displaying stream of pull requests. needs a bit more tlc

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# The Libraries Firehose 
+# The Libraries Firehose
 
 The Firehose is a streaming API that gives developers low latency access to Libraries’ activity without the overhead associated with polling a REST endpoint.
 
@@ -21,11 +21,39 @@ event: pkg
 data: {"platform":"Packagist","name":"miniframe/miniframe","version":"1.0.x-dev"}
 ```
 
+###
+
+Example event pull request event (JSON):
+
+```js
+{
+  "id": 25105,
+  "title": "works as a slide deck (with both arrows and keyboard buttons + can expand to show all slides if needed",
+  "issue_url": "https://github.com/ermlikeyeah/HMRC-Git-lessons/pull/2",
+  "body": "\r\n\r\n#24pr @ladieswhocode",
+  "state": "open",
+  "merged": false,
+  "created_at": "2015-12-05T15:49:50.000Z",
+  "repo_name": "ermlikeyeah/HMRC-Git-lessons",
+  "user_id": 10449,
+  "comments_count": 0,
+  "language": null,
+  "user": {
+    "uid": "8432520",
+    "nickname": "techforevil",
+    "name": null,
+    "blog": null,
+    "location": null
+  }
+}
+```
+
 From the standard:
 
 > The event stream is a simple stream of text data, which must be encoded using UTF-8. Each message is separated by a pair of newline characters. A colon as the first character of a line is, in essence, a comment, and is ignored.
 
 More info [here](https://developer.mozilla.org/en-US/docs/Server-sent_events/Using_server-sent_events#Event_stream_format).
+
 
 ## Try it
 
--- examples/liveview/main.js
+++ examples/liveview/main.js
@@ -1,14 +1,29 @@
-var evtSource = new EventSource('http://firehose.libraries.io/events');
+var evtSource = new EventSource('http://tf-firehose.herokuapp.com/events');
 
 var eventList = document.getElementById('events');
-evtSource.addEventListener('pkg', function(evt) {
-  var newElement = document.createElement("li");
-  
-  var pkg = JSON.parse(evt.data);
-  console.debug('pkg:', pkg);
 
-  var html = '['+ pkg.platform +'] '+ pkg.name +' '+ pkg.version;
-
-  newElement.innerHTML = html;
-  eventList.appendChild(newElement);
+evtSource.addEventListener('pullRequest', function(evt) {
+  var pr = JSON.parse(evt.data);
+  console.log(JSON.stringify(pr, null, 2));
+  render(pr)
 }, false);
+
+// example:
+$.getJSON("/pr.json", function( data ) {
+  console.log(JSON.stringify(data, null, 2));
+  render(data);
+});
+
+function render(pr) {
+  var newElement = document.createElement("li");
+  var src = 'https://avatars3.githubusercontent.com/u/' + pr.user.uid + '?v=3&s=40';
+  var img = '<img src="' + src  +'" width="40" height="40">';
+  var nick = '<strong><a href="https://github.com/' + pr.user.nickname + '" target="_blank">';
+  nick = nick + '@' + pr.user.nickname +'</a></strong>';
+  var pr_link = '<strong> <a href="' + pr.issue_url + '">' + pr.title + '</a> </strong>';
+  var time = moment(pr.created_at).fromNow();
+
+  var html = img + nick +' submitted Pull Request: ' + pr_link +' on ' + pr.repo_name +' '+ time;
+  newElement.innerHTML = html;
+  eventList.insertBefore(newElement, eventList.firstChild);
+}
