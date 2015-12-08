de4260f67d5f7efb0d8b25401abe507035897e74
nelsonic <contact.nelsonic+github@gmail.com>
2015-12-08 16:27:04 +0000

    adds demo of 24PR GitHub stream

--- examples/liveview/main.js
+++ examples/liveview/main.js
@@ -8,22 +8,23 @@ evtSource.addEventListener('pullRequest', function(evt) {
   render(pr)
 }, false);
 
-// example:
+// load examples
 $.getJSON("/pr.json", function( data ) {
-  console.log(JSON.stringify(data, null, 2));
-  render(data);
+  data.forEach(function(pr) { render(pr) });
 });
 
+// we could tidy this up using a view library, but it does what we need for now.
 function render(pr) {
   var newElement = document.createElement("li");
-  var src = 'https://avatars3.githubusercontent.com/u/' + pr.user.uid + '?v=3&s=40';
-  var img = '<img src="' + src  +'" width="40" height="40">';
-  var nick = '<strong><a href="https://github.com/' + pr.user.nickname + '" target="_blank">';
-  nick = nick + '@' + pr.user.nickname +'</a></strong>';
+  newElement.className += "card";
+  var src = 'https://avatars3.githubusercontent.com/u/' + pr.user.uid + '?v=3&s=60';
+  var img = '<div class="img-wrap"><img src="' + src  +'" width="60" height="60"></div>';
+  var nickname = '<div class="words"><strong><a href="https://github.com/' + pr.user.nickname + '" target="_blank">';
+  nickname += '@' + pr.user.nickname +'</a></strong>';
   var pr_link = '<strong> <a href="' + pr.issue_url + '">' + pr.title + '</a> </strong>';
-  var time = moment(pr.created_at).fromNow();
-
-  var html = img + nick +' submitted Pull Request: ' + pr_link +' on ' + pr.repo_name +' '+ time;
+  var pr_repo = '<a href="' + pr.issue_url + '">' + pr.repo_name + '</a></div>';
+  var html = img + nickname +' submitted Pull Request: ' + pr_link +'<br /> on ' + pr_repo;
+  // html += ' <em style="color:grey">' + moment(pr.created_at).fromNow() +'</em>';
   newElement.innerHTML = html;
-  eventList.insertBefore(newElement, eventList.firstChild);
+  eventList.insertBefore(newElement, eventList.firstChild); // prepend
 }
